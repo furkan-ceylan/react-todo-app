0908783aad51af6a957b5210cf79551242d3d8ec
furkan ceylan <ceylan.furkan@outlook.com>
2021-08-19 00:00:21 +0300

    Add dark theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import CreateTask from './CreateTask.js'
+import Tasks from './Tasks.js'
+import { createTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      type: 'dark',
+    },
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <CreateTask title="Hello everyone" />
+        <Tasks />
+      </div>
+    </ThemeProvider>
   )
 }
 
