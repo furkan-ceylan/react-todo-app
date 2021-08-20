63c3868b5687a7b87e4668ae08793d4d067aaa40
furkan ceylan <ceylan.furkan@outlook.com>
2021-08-20 21:54:12 +0300

    Add useState tasks object, addTask, deleteTask and toggleComplete

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
-import React from 'react'
+import { useState } from 'react'
 import CreateTask from './CreateTask.js'
 import Tasks from './Tasks.js'
 import { createTheme } from '@material-ui/core/styles'
 import { ThemeProvider } from '@material-ui/styles'
 
-function App() {
+const App = () => {
   const darkTheme = createTheme({
     palette: {
       type: 'dark',
     },
   })
 
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      taskName: 'Task 1',
+      completed: true,
+    },
+    {
+      id: 2,
+      taskName: 'Task 2',
+      completed: false,
+    },
+    {
+      id: 3,
+      taskName: 'Task 3',
+      completed: false,
+    },
+  ])
+
+  const addTask = (task) => {
+    //add task to tasks
+    setTasks([...tasks, task])
+  }
+
+  const deleteTask = (id) => {
+    //delete task from tasks
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    //toggle task from tasks
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    )
+  }
+
   return (
     <ThemeProvider theme={darkTheme}>
       <div className="App">
-        <CreateTask title="Hello everyone" />
-        <Tasks />
+        <CreateTask addTask={addTask} />
+        <Tasks
+          tasks={tasks}
+          deleteTask={deleteTask}
+          toggleComplete={toggleComplete}
+        />
       </div>
     </ThemeProvider>
   )
